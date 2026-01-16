581da0f66c40985e2ea61323aa5155e02e9b2ca1
Addison <zephyrpunch@gmail.com>
2026-01-16 00:00:16 -0500

    Tracking the files that would have otherwise been deleted

---
+++ App.py
@@ -0,0 +1,14 @@
+import streamlit as st
+from sklearn.ensemble import RandomForestRegressor
+import pandas as pd
+
+st.write("what's up guys")
+
+st.write("Below should be a dataframe")
+
+st.write(pd.DataFrame({
+    "column one" : [1, 2, 3, 4],
+    "column two" : ['a', 'b', 'c', 'd'] 
+}))
+
+st.write("Here is some new text")
